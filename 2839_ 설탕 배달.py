66116a86abaf8a3cd5310c88ef2a7460c4df72e0
YongjaeHyun <dltjrrbs2020@gmail.com>
2022-01-19 14:28:30 +0900

    Day - 19

--- 2839_ 설탕 배달.py
+++ 2839_ 설탕 배달.py
@@ -1,20 +1,12 @@
 n = int(input())
-div8, mod8 = divmod(n,8)
-div5, mod5 = divmod(mod8,5)
-div3, mod3 = divmod(mod8,3)
-if mod8 == 0:
-    print(div8*2)
-elif mod5 == 0:
-    div5, mod5 = divmod(n,5)
-    div3, mod3 = divmod(n,3)
-    print(div8*2 + div5)
-elif mod3 == 0:
-    print(div8*2 + div3)
-else:
-    div5, mod5 = divmod(n,5)
-    div3, mod3 = divmod(n,3)
-    if mod5 == 0:
-        print(div5)
-    elif mod3 == 0:
-        print(div3)
-    else: print(-1)
+ans = 0
+while n >= 0:
+    if n % 5 == 0:
+        ans += n//5
+        print(ans)
+        break
+    n -= 3
+    ans += 1
+if n < 0:
+    print(-1)
+    
