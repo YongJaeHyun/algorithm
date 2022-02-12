237614d823b7c893df326638ec92d4dd82c9f7b8
YongjaeHyun <dltjrrbs2020@kumoh.ac.kr>
2022-02-13 02:34:59 +0900

    Day - 41

---
+++ 15651_N과 M (3).py
@@ -0,0 +1,16 @@
+ans = []
+n, m = map(int, input().split())
+
+
+def func():
+    if len(ans) == m:
+        print(' '.join(map(str, ans)))
+
+    for i in range(1, n+1):
+        if len(ans) < m:
+            ans.append(i)
+            func()
+            ans.pop()
+
+
+func()
